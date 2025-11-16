b622f806cdc32e93e6f5cb3a2c6a7972af718f82
Andrew Beech <beech1842@gmail.com>
2025-11-16 13:20:45 +0000

    first search commit38

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,27 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
    const out = document.getElementById("search-output");
    if (out) {
-      //fetch("https://yellowmoss.github.io/sblog/search.txt")
-      fetch("search.txt")
-      .then(response => {
-         if (!response.ok) throw new Error('Network response was not ok');
-         return response.text();
-      })
-      .then(text => {
-         console.log('File loaded:', text);
-         document.getElementById('search-output').textContent = text;
+      fetch("search.json")
+      .then(response => response.json())
+      .then(data => {
+         console.log("Search JSON loaded:", data);
+         out.textContent = JSON.stringify(data, null, 2);
       })
       .catch(err => {
-         console.error('Error fetching file:', err);
-         document.getElementById('search-output').textContent = 'Failed to load file';
+         console.error("Error loading search.json:", err);
       });
-      //.then(response => response.json())
-      //.then(data => {
-      //   console.log("Search JSON loaded:", data);
-      //   out.textContent = JSON.stringify(data, null, 2);
-      //})
-      //.catch(err => {
-      //   console.error("Error loading search.json:", err);
-      //});
    }
 });
