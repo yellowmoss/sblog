819d20e990d5389a756a1c1a5061622448880bd3
Andrew Beech <beech1842@gmail.com>
2025-11-16 13:11:06 +0000

    first search commit35

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,37 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
    const out = document.getElementById("search-output");
    if (out) {
-      out.textContent += "hello worlllld"
-      out.textContent += "/n"
-      out.textContent += "\n"
+      //out.textContent += "hello worlllld"
+      //out.textContent += "/n"
+      //out.textContent += "\n"
 
-      out.textContent += {{ '/search.json' | relative_url }}
-      out.textContent += {{ '/search.json' | absolute_url }}
-      out.textContent += {{ '/search.json' }}
-
-      out.textContent += {{ site.baseurl | relative_url }}/search.json
-      out.textContent += {{ site.baseurl | absolute_url }}/search.json
-      out.textContent += {{ site.baseurl }}/search.json
-
-      out.textContent += {{ site.url | relative_url }}/search.json
-      out.textContent += {{ site.url | absolute_url }}/search.json
-      out.textContent += {{ site.url }}/search.json
-
-
+      //out.textContent += {{ '/search.json' | relative_url }}
+      //out.textContent += {{ '/search.json' | absolute_url }}
+      //out.textContent += {{ '/search.json' }}
+      //out.textContent += {{ site.baseurl | relative_url }}/search.json
+      //out.textContent += {{ site.baseurl | absolute_url }}/search.json
+      //out.textContent += {{ site.baseurl }}/search.json
+      //out.textContent += {{ site.url | relative_url }}/search.json
+      //out.textContent += {{ site.url | absolute_url }}/search.json
+      //out.textContent += {{ site.url }}/search.json
       //fetch('{{ "/search.txt" | relative_url }}')
-      //fetch("/search.txt")
-      //.then(response => {
-      //   if (!response.ok) throw new Error('Network response was not ok');
-      //   return response.text();
-      //})
-      //.then(text => {
-      //   console.log('File loaded:', text);
-      //   document.getElementById('search-output').textContent = text;
-      //})
-      //.catch(err => {
-      //   console.error('Error fetching file:', err);
-      //   document.getElementById('search-output').textContent = 'Failed to load file';
-      //});
+      fetch("/search.txt")
+      .then(response => {
+         if (!response.ok) throw new Error('Network response was not ok');
+         return response.text();
+      })
+      .then(text => {
+         console.log('File loaded:', text);
+         document.getElementById('search-output').textContent = text;
+      })
+      .catch(err => {
+         console.error('Error fetching file:', err);
+         document.getElementById('search-output').textContent = 'Failed to load file';
+      });
       //.then(response => response.json())
       //.then(data => {
       //   console.log("Search JSON loaded:", data);
